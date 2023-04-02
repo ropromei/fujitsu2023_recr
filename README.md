9498955c51086ac7fd287ad19bda791725d3da44
Reio Opromei <ropromei@gmail.com>
2023-04-02 23:30:19 +0300

    API endpoint for weather data and README update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # fujitsu2023_recr
 Fujitsu Estonia 2023 recruitment exercise.
+
+## Used stack
+The project is being built on Maven+Spring stack, code
+syntax level is Java v17. H2 database engine is being used.
+It is highly suggested that the default configuration
+(especially for the database) is being changed (located 
+in **application.yml** file in service folder).
+
+## Current state
+The project creates an H2 database and
+fetches weather data from the weather service of Estonian
+Environment Agency.
+
+The Weather data gets fetched from
+https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php.
+The cron job is scheduled to pull the latest weather data
+at the 15th minute of every hour.
+
+It is possible to get data from the API server. When
+sending a GET-request to address http://localhost:8080/api/v1/weather,
+the list of latest weather data pulls is being shown. The amount
+of shown weather observations shown is determined by
+the amount of weather stations in the database. In most
+cases it should ensure that the latest observations from
+all the stations we have inserted are being listed.
--- src/main/java/com/ropromei/fujitsu2023_recr/Fujitsu2023RecrApplication.java
+++ src/main/java/com/ropromei/fujitsu2023_recr/Fujitsu2023RecrApplication.java
@@ -2,8 +2,10 @@ package com.ropromei.fujitsu2023_recr;
 
 import com.ropromei.fujitsu2023_recr.BLL.WeatherData;
 import com.ropromei.fujitsu2023_recr.DAL.WeatherDataRepository;
+import com.ropromei.fujitsu2023_recr.DAL.WeatherStationRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,12 @@ import java.util.List;
 public class Fujitsu2023RecrApplication {
 
 	private final WeatherDataRepository weatherDataRepository;
+	private final WeatherStationRepository weatherStationRepository;
 
-	public Fujitsu2023RecrApplication(WeatherDataRepository weatherDataRepository) {
+	public Fujitsu2023RecrApplication(WeatherDataRepository weatherDataRepository,
+									  WeatherStationRepository weatherStationRepository) {
 		this.weatherDataRepository = weatherDataRepository;
+		this.weatherStationRepository = weatherStationRepository;
 	}
 
 	public static void main(String[] args) {
@@ -28,6 +33,16 @@ public class Fujitsu2023RecrApplication {
 	@GetMapping("")
 	public GreetResponse hello() {
 		return new GreetResponse("Hello World!");
+	}
+
+	@GetMapping("/weather")
+	public List<WeatherData> getWeatherData() {
+		long weatherStationsCount = weatherStationRepository.count();
+		return weatherDataRepository
+				.findAll(Sort.by(Sort.Direction.DESC, "timestamp"))
+				.stream()
+				.limit(weatherStationsCount)
+				.toList();
 	}
 
 	record GreetResponse(String greet) {}
