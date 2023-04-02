0577d98db688b08a499024760ebbdd6ca0334619
Reio Opromei <ropromei@gmail.com>
2023-04-02 14:44:21 +0300

    initial commit

---
+++ src/main/java/com/ropromei/fujitsu2023_recr/Fujitsu2023RecrApplication.java
@@ -0,0 +1,13 @@
+package com.ropromei.fujitsu2023_recr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fujitsu2023RecrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fujitsu2023RecrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ropromei/fujitsu2023_recr/Fujitsu2023RecrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ropromei.fujitsu2023_recr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fujitsu2023RecrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
